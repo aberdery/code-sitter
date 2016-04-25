003293e8fd1e11f945a01da5ca580b3052d623ed
Alexandre BERDERY <alexandre.berdery@provenrun.com>
2016-04-25 12:48:04 +0200

    Adapt test_suite.json to latest format
    @config time, test_suite.json is now used with tests_lib.json found in
    kernelC/tests folder in order to build match between public test's id and
    private test path.

--- code_sitter_cmd.py
+++ code_sitter_cmd.py
@@ -2,6 +2,13 @@
 import sys, os, threading, json, pexpect
 from subprocess import Popen, PIPE
 
+TEST_SESSION={
+    "tests":0,
+    "pass":0,
+    "fail":0,
+    "inconclusive":0,
+    "not_run":0 }
+
 class RunCmd(threading.Thread):
     def __init__(self, cmd, outpipe, cwd, timeout):
         threading.Thread.__init__(self)
@@ -47,69 +54,114 @@ def runTests(tests, path, prefix, emu_bin=None, emu_args=None):
     # Run all tests one by one
     # For each test get end infos
     # Compute results
+    global TEST_SESSION
     prefix = '  ' + prefix
-    num_tests=0
-    num_pass=0
-    num_fail=0
-    num_inc=0
-    num_notrun=0
     #default timeout: 20s
     timeout = 20
 
     f=open(os.path.join('default_tests_log.txt'), 'w')
-    for domain in tests['domains']:
-        for test in domain['tests']:
-            num_tests+=1
-            tcmd = test['name']
-            if test['args'] != 'none':
-                tcmd += ' ' + test['args']
-            t = timeout
-            try:
-                t = test['timeout']
-            except:
-                pass
-            p = pexpect.spawn(emu_bin, emu_args, cwd=path, logfile=f)
-            p.expect('ProvenCore\[', timeout=t)
-            p.sendline(tcmd)
-            idx = p.expect(['<--- PNC TEST STATUS:', 'Command failed', pexpect.TIMEOUT], timeout=t)
-            if idx == 1:
-                print "%sTest %s: NOT RUN" % (prefix, test['name'])
-                num_notrun+=1
+    for test in tests:
+        TEST_SESSION['tests']+=1
+        tcmd = test['path']
+        if test['args'] != 'none':
+            tcmd += ' ' + test['args']
+        t = timeout
+        try:
+            t = test['timeout']
+        except:
+            pass
+        p = pexpect.spawn(emu_bin, emu_args, cwd=path, logfile=f)
+        p.expect('ProvenCore\[', timeout=t)
+        p.sendline(tcmd)
+        idx = p.expect(['<--- PNC TEST STATUS:', 'Command failed', pexpect.TIMEOUT], timeout=t)
+        if idx == 1:
+            print "%sTest %s: NOT RUN" % (prefix, test['path'])
+            TEST_SESSION['not_run']+=1
+        elif idx == 2:
+            print "%sTest %s: TIMEOUT" % (prefix, test['path'])
+            TEST_SESSION['fail']+=1
+        else:
+            idx=p.expect(['PASS', 'FAIL', 'INCONCLUSIVE'])
+            if idx == 0:
+                print "%sTest %s: PASS" % (prefix, test['path'])
+                TEST_SESSION['pass']+=1
+            elif idx == 1:
+                print "%sTest %s: FAIL" % (prefix, test['path'])
+                TEST_SESSION['fail']+=1
             elif idx == 2:
-                print "%sTest %s: TIMEOUT" % (prefix, test['name'])
-                num_fail+=1
-            else:
-                idx=p.expect(['PASS', 'FAIL', 'INCONCLUSIVE'])
-                if idx == 0:
-                    print "%sTest %s: PASS" % (prefix, test['name'])
-                    num_pass+=1
-                elif idx == 1:
-                    print "%sTest %s: FAIL" % (prefix, test['name'])
-                    num_fail+=1
-                elif idx == 2:
-                    print "%sTest %s: INCONCLUSIVE" % (prefix, test['name'])
-                    num_inc+=1
-
+                print "%sTest %s: INCONCLUSIVE" % (prefix, test['path'])
+                TEST_SESSION['inconclusive']+=1
     f.close()
-    return num_tests, num_pass, num_fail, num_inc, num_notrun
+    return TEST_SESSION
 
-def configTests(tests, path, prefix=None):
+def lookUpTestPath(id, tests_lib):
+    # TODO: clever parsing of tests lib in python ??
+    path = None
+    for domain in tests_lib['domains']:
+        if domain['id'] == id[0]:
+            for case in domain['cases']:
+                if id == case['id']:
+                    path = case['dir']
+    return path
+
+def configTests(tests, root, prefix=None):
+    global TEST_SESSION
+
     # Update config.mk to build init with only shell
-    afile = os.path.join(path, 'config.mk')
-    f = open(afile, 'a')
-    f.write('FEATURES += INIT_SHELL\n')
-    f.close()
+    afile = os.path.join(root, 'config.mk')
+    fp = open(afile, 'a')
+    fp.write('FEATURES += INIT_SHELL\n')
+    fp.close()
     print "%s  config.mk updated successfully."%prefix
 
+    # Retreive tests library
+    tests_lib_file = os.path.join(root, 'tests', 'tests_lib.json')
+    try:
+        fp = open(tests_lib_file)
+        tests_lib = json.load(fp)
+        fp.close()
+    except Exception as e:
+        print "Unable to read tests lib file '%s': %s\n"%(tests_lib_file, str(e))
+        sys.exit(-1)
+
+    # Build list of tests to run looking for matching id between test suite and
+    # tests lib in order to find out test's path...
+    tmplist=[]
+    tests_list=[]
+    for test in tests['tests']:
+        found = 0
+        path = None
+        for domain in tests_lib['domains']:
+            if domain['id'] == test['id'][0]:
+                for case in domain['cases']:
+                    if test['id'] == case['id']:
+                        found = 1
+                        path = case['dir']
+                        if path not in tmplist:
+                            tmplist.append(case['dir'])
+                        break
+            if found == 1:
+                break
+        if found == 0:
+            print "%s  test: [%s] is not a valid/known test."%(prefix, test['id'])
+            TEST_SESSION['tests']+=1
+            TEST_SESSION['not_run']+=1
+        else:
+            test['path'] = os.path.join('tests', path, 'test')
+            tests_list.append(test)
+
     # Create pnc_tests.mk containing all test domains to build/link in image
-    tfile = os.path.join(path, 'pnc_tests.mk')
-    subcommand("rm -f %s"%tfile, ['rm', '-f', tfile], path, "%s  "%prefix, display=False)
-    f = open(tfile, 'w')
-    f.write('PNC_TESTS := \\\n')
-    for domain in tests['domains']:
-        f.write('  '+domain['name']+'\\\n')
-    f.close()
-    print "%s  pnc_tests.mk created successfully."%prefix
+    tfile = os.path.join(root, 'pnc_tests.mk')
+    subcommand("rm -f %s"%tfile, ['rm', '-f', tfile], root, "%s  "%prefix, display=False)
+    if len(tmplist) != 0:
+        f = open(tfile, 'w')
+        f.write('PNC_TESTS := \\\n')
+        for path in tmplist:
+            f.write('  '+path+'\\\n')
+        f.close()
+        print "%s  pnc_tests.mk created successfully."%prefix
+
+    return tests_list
 
 def setup_toolchain(prefix):
     # check for <prefix>-gcc as direct path or in os.environ["PATH"]
@@ -140,7 +192,7 @@ def build_recipe(path, name, target, config, run_qemu, prefix, tests=None):
     subcommand("make distclean", ['make', 'distclean'], full_path, "%s  "%prefix)
     subcommand("make %s %s"%(target, cross_compile), ['make', target, cross_compile], full_path, "%s  "%prefix)
     if tests != None:
-        configTests(tests, full_path, prefix)
+        tests_list = configTests(tests, full_path, prefix)
     subcommand("make config %s"%cross_compile, ['make', 'config', cross_compile], full_path, "%s  "%prefix)
     subcommand("make programs %s"%cross_compile, ['make', 'programs', cross_compile], full_path, "%s  "%prefix)
     subcommand("make %s"%cross_compile, ['make', 'all', cross_compile], full_path, "%s  "%prefix)
@@ -159,8 +211,9 @@ def build_recipe(path, name, target, config, run_qemu, prefix, tests=None):
             RunCmd(qemu_cmd, PIPE, full_path, 5).Run()
         else:
             print "%sRunning tests for %s:%s"%(prefix, name,target)
-            (n, p, f, i, nr)=runTests(tests, full_path, prefix, emu_bin=qemu_bin, emu_args=config['qemu-args'].split(' '))
-            print "%sSUMMARY: %d tests, %d PASS, %d FAIL, %d INCONCLUSIVE, %d NOT RUN" % (prefix, n, p, f, i, nr)
+            session=runTests(tests_list, full_path, prefix, emu_bin=qemu_bin, emu_args=config['qemu-args'].split(' '))
+            print "%sSUMMARY: %d tests, %d PASS, %d FAIL, %d INCONCLUSIVE, %d NOT RUN" % \
+            (prefix, session['tests'], session['pass'], session['fail'], session['inconclusive'], session['not_run'])
 
 def setup_recipe(path, name, prefix):
     full_path = os.path.join(path, name)
